44344db59d817678ec299d38a097569cd468fe01
RU-Developer <dev.ru@kakao.com>
2022-09-15 22:17:34 +0900

    회원가입 기능 추가 및 로그인 오류 수정

--- app/src/main/java/com/example/capstonedesign/RegisterActivity.java
+++ app/src/main/java/com/example/capstonedesign/RegisterActivity.java
@@ -2,7 +2,6 @@ package com.example.capstonedesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
