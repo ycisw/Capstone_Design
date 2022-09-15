296f1bfdc4ff921e66fdaaba532dcbd6fe2a0586
swsw5 <sw5920@naver.com>
2022-09-15 09:51:00 +0900

    ...

---
+++ app/src/main/java/com/example/capstonedesign/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.capstonedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public abstract class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText ID;
+        ID = findViewById(R.id.name);
+        EditText PW = findViewById(R.id.password);
+        Button login = findViewById(R.id.login);
+        String id = ID.getText().toString();
+        String pw = PW.getText().toString();
+        String pl = "1234";
+        String plz = "4321";
+        Intent intent = new Intent(MainActivity.this, homepage.class);
+        login.setOnClickListener(view -> {
+                if (id.equals(pl) && pw.equals(plz)) {
+
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "취소하였습니다.", Toast.LENGTH_SHORT).show();
+                }
+        });
+
+    }
+    }
