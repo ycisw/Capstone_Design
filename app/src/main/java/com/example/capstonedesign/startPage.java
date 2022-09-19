4a4dca6b7eeec7283767501df8fcdbdf0b7ef6b7
Ballpenman <qhfvpsdls@gmail.com>
2022-09-19 21:45:33 +0900

    초기화면 학부모,강사 선택

---
+++ app/src/main/java/com/example/capstonedesign/startPage.java
@@ -0,0 +1,25 @@
+package com.example.capstonedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class startPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+
+        ImageButton parents =findViewById(R.id.parents);
+        ImageButton teacher =findViewById(R.id.teacher);
+
+        parents.setOnClickListener(v->
+            startActivity(new Intent(this,MainActivity.class)));
+
+        teacher.setOnClickListener(v->
+            startActivity(new Intent(this,MainActivity.class)));
+    }
+}
