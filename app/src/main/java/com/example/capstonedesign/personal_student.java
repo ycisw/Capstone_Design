9d2785bb72347a223b35c8f302d6e50214ea1ea6
Cho_jeong_woo <jungu1256@naver.com>
2022-09-28 18:02:26 +0900

    학생 개인용 페이지(일단 틀만)

---
+++ app/src/main/java/com/example/capstonedesign/personal_student.java
@@ -0,0 +1,41 @@
+package com.example.capstonedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class personal_student extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_student);
+
+        ListView check_log =  findViewById(R.id.check_log);
+        ListView check_date = findViewById(R.id.check_date);
+        TextView attendace_date = findViewById(R.id.attendace_date);
+        TextView attendace_record = findViewById(R.id.attendace_record);
+
+
+        List<String> data1 = new ArrayList<>();
+        List<String> data2 = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data1); //현재 액티비티
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data2);
+        // 데이터를 담은 어댑터 생성
+        check_date.setAdapter(adapter);
+        check_log.setAdapter(adapter2);
+
+        data1.add("2022년 09월 28일 8시 03분");
+        data1.add("2022년 09월 28일 8시 03분");
+        data2.add("출석");
+        data2.add("출석");
+
+        adapter.notifyDataSetChanged(); //
+        adapter2.notifyDataSetChanged();
+    }
+}
