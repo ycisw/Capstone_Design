e93ead205f1c79f709ac690b3359631e518fdb42
swsw5 <sw5920@naver.com>
2022-10-06 10:01:39 +0900

    학생 개인 출석 기록 보기 api 연결

--- app/src/main/java/com/example/capstonedesign/personal_student.java
+++ app/src/main/java/com/example/capstonedesign/personal_student.java
@@ -11,6 +11,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.capstonedesign.server.domain.attendance.Attendance;
+import com.example.capstonedesign.server.domain.attendance.AttendanceStudentResult;
+import com.example.capstonedesign.server.domain.network.NetworkLogic;
+import com.example.capstonedesign.server.service.AttendanceService;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,20 +37,21 @@ public class personal_student extends AppCompatActivity {
 
         List<String> data1 = new ArrayList<>();
         List<String> data2 = new ArrayList<>();
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data1); //현재 액티비티
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data2);
         // 데이터를 담은 어댑터 생성
         check_date.setAdapter(adapter);
         check_log.setAdapter(adapter2);
-
-        data1.add("2022년 09월 28일 20시 00분");
-        data1.add("2022년 09월 27일 20시 00분");
-        data1.add("2022년 09월 26일 20시 00분");
-        data2.add("출석");
-        data2.add("출석");
-        data2.add("출석");
-        adapter.notifyDataSetChanged(); //
-        adapter2.notifyDataSetChanged();
+//
+//        data1.add("2022년 09월 28일 20시 00분");
+//        data1.add("2022년 09월 27일 20시 00분");
+//        data1.add("2022년 09월 26일 20시 00분");
+//        data2.add("출석");
+//        data2.add("출석");
+//        data2.add("출석");
+//        adapter.notifyDataSetChanged(); //
+//        adapter2.notifyDataSetChanged();
 
         check_log.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -63,5 +70,30 @@ public class personal_student extends AppCompatActivity {
 
             }
         });
+
+        AttendanceService.studentForm(studentId, new NetworkLogic<AttendanceStudentResult>(
+                result -> {
+                    List<Attendance> attendances = result.getAttendances();
+                    for (Attendance attendance : attendances) {
+                        LocalDateTime inTime = attendance.getInTime();
+                        if (inTime == null) {
+                            data1.add("등원하지 않았음");
+                            data2.add("결석");
+                            continue;
+                        }
+                        data1.add(inTime.getYear() + "년 " + inTime.getMonthValue() + "월 " + inTime.getDayOfMonth() + "일 "
+                                + inTime.getHour() + "시 " + inTime.getMinute() + "분");
+                        data2.add("출석");
+                    }
+
+                    refresh(adapter, adapter2);
+                },
+                result -> {}
+        ));
+    }
+
+    private void refresh(ArrayAdapter<String> adapter, ArrayAdapter<String> adapter2) {
+        adapter.notifyDataSetChanged();
+        adapter2.notifyDataSetChanged();
     }
 }
