138c6ddc5233016eaf1568843e948a77834be139
ballpenman <qhfvpsdls@gmail.com>
2022-10-06 10:28:10 +0900

    학생정보 수정 다이얼로그

--- app/src/main/java/com/example/capstonedesign/StudentProfile.java
+++ app/src/main/java/com/example/capstonedesign/StudentProfile.java
@@ -6,19 +6,24 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.capstonedesign.server.domain.network.NetworkLogic;
+import com.example.capstonedesign.server.domain.parent.Parent;
+import com.example.capstonedesign.server.domain.student.Student;
 import com.example.capstonedesign.server.domain.student.StudentParent;
 import com.example.capstonedesign.server.service.StudentService;
 import com.example.capstonedesign.student.ListViewAdapter;
 import com.example.capstonedesign.student.ListViewItem;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class StudentProfile extends AppCompatActivity {
     Dialog dialog;
+    StudentParent student1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +37,14 @@ public class StudentProfile extends AppCompatActivity {
         //학생정보 출력
         long sid = getIntent().getLongExtra("sid",0);
         TextView sname = findViewById(R.id.sname);
-        StudentParent student1 = new StudentParent();
+        student1 = new StudentParent();
 
         StudentService.profile(sid, new NetworkLogic<StudentParent>(
                 studentParent -> {
                     String name = studentParent.getStudent().getName();
                     student1.setStudent(studentParent.getStudent());
-                    student1.setParent(studentParent.getParent());
-
                     sname.setText(student1.getStudent().getName());
-
-                    Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+                    student1.setParent(studentParent.getParent());
                 },
                 none -> {}
         ));
@@ -66,6 +68,18 @@ public class StudentProfile extends AppCompatActivity {
         dialog.show();
 
         Button back = dialog.findViewById(R.id.back4);
+        Button update_student = dialog.findViewById(R.id.update_student);
+        EditText update_name = dialog.findViewById(R.id.update_name);
+        EditText update_pname = dialog.findViewById(R.id.update_pname);
+        EditText update_pphone = dialog.findViewById(R.id.update_pphone);
+
+        update_student.setOnClickListener(v->{
+            StudentService.update(new StudentParent(new Student(student1.getStudent().getId(), update_name.getText().toString(), "01090663150", 10000L, LocalDate.now(), -1L, -1L),
+                new Parent(-1L, update_pname.getText().toString(), update_pphone.getText().toString())), new NetworkLogic<>(
+                none -> {},
+                none -> {}
+        ));
+        });
 
         back.setOnClickListener(v->{
             dialog.dismiss();
