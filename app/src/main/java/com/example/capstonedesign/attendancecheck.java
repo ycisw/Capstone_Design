bda6dd792c0ddeda3173846396e0c2fec2ac6713
RU-Developer <dev.ru@kakao.com>
2022-11-10 18:21:55 +0900

    attendancecheck RecyclerView 구현

--- app/src/main/java/com/example/capstonedesign/attendancecheck.java
+++ app/src/main/java/com/example/capstonedesign/attendancecheck.java
@@ -1,6 +1,10 @@
 package com.example.capstonedesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -22,99 +27,74 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class attendancecheck extends AppCompatActivity{
-    ArrayAdapter<String> adapter1;
-    ArrayAdapter<String> adapter2;
-    ArrayList<String> listItem;
-    ArrayList<String> listItem2;
-    List<StudentParent> listItem3;
-    HashSet<StudentParent> checkSet;
-    String studentName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendancecheck);
-        Button goButton = findViewById(R.id.goToTheHome);
-        Button checkButton = findViewById(R.id.checkBT);
-        ImageButton back_btn = findViewById(R.id.back_btn);
+        Button goButton = findViewById(R.id.goToTheHome); //하원 버튼
+        Button checkButton = findViewById(R.id.checkBT); //등원 버튼
+        ImageButton back_btn = findViewById(R.id.back_btn); //뒤로가기 버튼
 
-        listItem = new ArrayList<String>();
-        listItem2 = new ArrayList<String>();
-        listItem3 = new ArrayList<>();
+        ArrayList<AttendanceCheckModel> arrayList = new ArrayList<>(); //adapter에 들어가는 리스트
 
-        checkSet = new HashSet<>();
+        //recycler view 설정
+        RecyclerView recyclerView = findViewById(R.id.attendance_check_recyclerview); //recyclerview
+        AttendanceCheckAdapter adapter = new AttendanceCheckAdapter(this, arrayList); //adapter
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)); //이것들은 그냥 디자인 매니저들
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setAdapter(adapter); //adapter 세팅
 
-        adapter1 = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,listItem);
-        adapter2 = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_multiple_choice,listItem2);
-        ListView listView = findViewById(R.id.listView1);
-        listView.setAdapter(adapter1);
-        ListView listView2 = findViewById(R.id.listView2);
-        listView2.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        listView2.setAdapter(adapter2);
-
-        listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            // 콜백매개변수는 순서대로 어댑터뷰, 해당 아이템의 뷰, 클릭한 순번, 항목의 아이디
-
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if (checkSet.contains(listItem.get(i))) {
-                    checkSet.remove(listItem.get(i));
-                    return;
-                }
-                checkSet.add(listItem3.get(i));
-            }
-        });
+        //뒤로가기 이미지버튼
         back_btn.setOnClickListener(v->{
             finishAffinity();
             startActivity(new Intent(this, homepage.class));
         });
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(attendancecheck.this, personal_student.class);
-                intent.putExtra("studentId", listItem3.get(i).getStudent().getId());
-                startActivity(intent);
-            }
-        });
 
-        checkButton.setOnClickListener(v->{
-            List<Long> studentIdList = new LinkedList<>();
-            for (StudentParent checked : checkSet) {
-                studentIdList.add(checked.getStudent().getId());
-                studentName += checked.getStudent().getName() + ",";
-            }
-            Toast.makeText(this,studentName + "학생들이 등원했습니다.",Toast.LENGTH_SHORT).show();
-            AttendanceService.attendanceToday(studentIdList, new NetworkLogic<>(
-                    none -> {},
-                    none -> {}
-            ));
-        });
-        // 하원 버튼 눌렀을때
-        goButton.setOnClickListener(v->{
-            List<Long> studentIdList = new LinkedList<>();
-            for (StudentParent checked : checkSet) {
-                studentIdList.add(checked.getStudent().getId());
-                studentName += checked.getStudent().getName() + ",";
-            }
-            Toast.makeText(this,studentName + "학생들이 하원했습니다.",Toast.LENGTH_SHORT).show();
-
-
-            AttendanceService.leaveAcademyToday(studentIdList, new NetworkLogic<>(
-                    none -> {},
-                    none -> {}
-            ));
-        });
-        // 등원버튼 눌렀을때
+        //학생 데이터 불러오기
         AttendanceService.studentParentForAttendances(new NetworkLogic<List<StudentParent>>(
                 result -> {
                     //성공
+                    //학생과 부모 정보를 가져와서 어뎁터에 추가
                     for (StudentParent studentParent : result) {
-                        add(studentParent);
+                        arrayList.add(new AttendanceCheckModel(studentParent));
                     }
-                    refresh();
+                    adapter.notifyDataSetChanged();
                 },
                 result -> {}
         ));
+
+        //등원 버튼 눌렀을 때
+        checkButton.setOnClickListener(v -> {
+            List<Long> studentIdList = new ArrayList<>();
+            StringBuilder sb = new StringBuilder(); //토스트 출력때문에 사용
+            for (StudentParent checked : adapter.getCheckSet()) { //체크된 요소들 어뎁터에 있음
+                studentIdList.add(checked.getStudent().getId());
+                sb.append(checked.getStudent().getName());
+                sb.append(",");
+            }
+            Toast.makeText(this,sb.toString() + "학생들이 등원했습니다.",Toast.LENGTH_SHORT).show();
+            AttendanceService.attendanceToday(studentIdList, new NetworkLogic<>( //등원처리
+                    none -> {},
+                    none -> {}
+            ));
+        });
+
+        // 하원 버튼 눌렀을 때
+        goButton.setOnClickListener(v->{
+            List<Long> studentIdList = new ArrayList<>();
+            StringBuilder sb = new StringBuilder();
+            for (StudentParent checked : adapter.getCheckSet()) {
+                sb.append(checked.getStudent().getName());
+                sb.append(",");
+            }
+            Toast.makeText(this,sb.toString() + "학생들이 하원했습니다.",Toast.LENGTH_SHORT).show();
+            AttendanceService.leaveAcademyToday(studentIdList, new NetworkLogic<>( //하원처리
+                    none -> {},
+                    none -> {}
+            ));
+        });
     }
 
     @Override
@@ -122,16 +102,4 @@ public class attendancecheck extends AppCompatActivity{
         finishAffinity();
         startActivity(new Intent(this, homepage.class));
     }
-
-    private void refresh() {
-        adapter1.notifyDataSetChanged();
-        adapter2.notifyDataSetChanged();
-    }
-
-    private void add(StudentParent studentParent) {
-        listItem3.add(studentParent); //실제 데이터
-        listItem.add(studentParent.getStudent().getName()); //학생 이름 출력 하는 부분
-        listItem2.add(""); //체크박스 옆에 텍스트 없애기
-    }
-
 }
