6860488828a003cfc5fd404d59fbbeb6ab24300e
Cho_jeong_woo <jungu1256@naver.com>
2022-11-24 10:15:31 +0900

    오류 수정중

--- app/src/main/java/com/example/capstonedesign/InquireChild.java
+++ app/src/main/java/com/example/capstonedesign/InquireChild.java
@@ -11,7 +11,6 @@ import android.widget.ListView;
 import com.example.capstonedesign.server.domain.network.NetworkLogic;
 import com.example.capstonedesign.server.domain.student.Student;
 import com.example.capstonedesign.server.service.ParentService;
-import com.example.capstonedesign.student.InquireChildAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,10 +28,7 @@ public class InquireChild extends AppCompatActivity {
         List<String> student_info = new ArrayList<>();
         List<Student> subList = new ArrayList<>();
 
-       // ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,student_info);
-        InquireChildAdapter adapter;
-        adapter = new InquireChildAdapter();
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,student_info);
         student_info_list.setAdapter(adapter);
 
         student_info_list.setOnItemClickListener((adapterView, view, i, l) -> {
@@ -50,16 +46,19 @@ public class InquireChild extends AppCompatActivity {
         });
     }
 
-    private void addChildren(List<String> student_info, List<Student> subList, InquireChildAdapter adapter) {
+    private void addChildren(List<String> student_info, List<Student> subList, ArrayAdapter<String> adapter) {
         ParentService.children(new NetworkLogic<List<Student>>(students -> {
             for (Student student : students) {
-                adapter.addItem(student.getName());
+                add(student_info, subList, student);
             }
             adapter.notifyDataSetChanged();
         }, none -> {}));
     }
 
-
+    private void add(List<String> student_info, List<Student> subList, Student student) {
+        student_info.add(student.getName());
+        subList.add(student);
+    }
 
     @Override
     public void onBackPressed() {
